89b92319837791032966fb27cd50e7dff1810165
abdelhak zekri <azekri968@gmail.com>
2024-02-02 19:24:45 +0100

    Update tarjet.php

--- fonction/tarjet.php
+++ fonction/tarjet.php
@@ -13,6 +13,12 @@ $nb=intval($nb["idtrajet"]+1);
 // echo $nb;
 $sql="INSERT INTO trajet (idtrajet,iduser,pick_att,pick_long,drop_att,drop_long) VALUES ('$nb', '$username', '$pick_att', '$pick_long', '$drop_att', '$drop_long')";
 $re=$conn->query($sql);
-print json_encode($nb);
+$sql="SELECT idtrajet,pick_att,pick_long,drop_att,drop_long from trajet where idtrajet=$nb";
+$re=$conn->query($sql);
+$r=$re->fetch_assoc();
+//var_dump($r); 
+$re=array();
+$re=$r;
+print json_encode($re);
 
 ?>
