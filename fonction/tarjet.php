d7f34eec78affcf59d2bed3441c5ef5cafd31077
abdelhak zekri <azekri968@gmail.com>
2024-02-02 17:05:59 +0100

    Update tarjet.php

--- fonction/tarjet.php
+++ fonction/tarjet.php
@@ -1,6 +1,9 @@
 <?php
-$drop=36.706713395650354;
-$pick=36.66984289615055;
-
-header("location:http://dev.virtualearth.net/REST/v1/locationrecog/36.706713395650354,36.66984289615055?key=AnBg1zAkil1YotryDJPI3f73rIsfIBtk6YMFrPIIzMmW-OVo54wmpaljHrJlZV4l&output=json")
+$drop_att=$_GET["drop_att"];
+$drop_long=$_GET["drop_long"];
+$pick_att=$_GET["pick_att"];
+$pick_long=$_GET["pick_long"];
+$hello="location:http://dev.virtualearth.net/REST/v1/locationrecog/".$drop_att.",".$drop_long;
+echo $hello;
+//header("location:http://dev.virtualearth.net/REST/v1/locationrecog/36.723714274679814,2.998576911884161?key=AnBg1zAkil1YotryDJPI3f73rIsfIBtk6YMFrPIIzMmW-OVo54wmpaljHrJlZV4l&output=json")
 ?>
