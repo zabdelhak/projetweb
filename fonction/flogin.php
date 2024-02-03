07e18c8e5cd347e22e471bc64fe6becb9ba847e1
abdelhak zekri <azekri968@gmail.com>
2024-02-03 12:57:53 +0100

    Update flogin.php

--- fonction/flogin.php
+++ fonction/flogin.php
@@ -2,10 +2,11 @@
 session_start();
   if(isset($_POST["email"]) && isset($_POST["mdp"]))
  {   include("condb.php");
-        $email=mysqli_real_escape_string( $conn,htmlspecialchars($_POST['email']));
+      $email=mysqli_real_escape_string( $conn,htmlspecialchars($_POST['email']));
      $mdp=mysqli_real_escape_string( $conn,htmlspecialchars($_POST['mdp']));
      if($email!=="" && $mdp!=="")
      {
+      echo("hello");
       $date="UPDATE user set last_cnx=sysdate() WHERE (email='$email'and mdp='$mdp')";
       $re=$conn->query($date);
       $row="SELECT iduser,user_name,user_fname,cel_number from user where (email='$email' and mdp='$mdp')";
@@ -14,17 +15,19 @@ session_start();
      if($rows!=0)
      {
       $_SESSION["username"]=$rows['iduser'];
-      echo $_SESSION["username"];
-      header("location: http://127.0.0.1:81/projetweb/pagep.php");
+      //echo $_SESSION["username"];
+      header("location: http://127.0.0.1:81/projetweb/chauffeur/pagep.php");
      }
      else
      {
-      //header("location: http://127.0.0.1:81/projetweb/login.php?er=1");
+      
+      header("location: http://127.0.0.1:81/projetweb/chauffeur/login.php?er=1");
      }
    }
    else
-   {
-      //header("location: http://127.0.0.1:81/projetweb/login.php?ep=1");
+   {  
+      header("location: http://127.0.0.1:81/projetweb/chauffeur/login.php?ep=1");
+      //chauffeur\login.php
    }
 }
 ?>
