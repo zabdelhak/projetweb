9032215a6d24282ea25100f0252171b4d05245fc
abdelhak zekri <azekri968@gmail.com>
2024-02-03 12:57:56 +0100

    Update session.php

--- fonction/session.php
+++ fonction/session.php
@@ -1,4 +1,4 @@
 <?php
     include("condb.php");
-    if(!(isset($_SESSION["username"]))){header("location: http://127.0.0.1:81/projetweb/login.php");die();}
+    if(!(isset($_SESSION["username"]))){header("location: http://127.0.0.1:81/projetweb/chauffeur/login.php");die();}
 ?>
