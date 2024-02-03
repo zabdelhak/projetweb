3015d90acef06b6fed7a0d96a897645bee3e0fd0
abdelhak zekri <azekri968@gmail.com>
2024-02-03 14:15:58 +0100

    Update setdate.php

--- fonction/setdate.php
+++ fonction/setdate.php
@@ -1,8 +1,17 @@
 <?php
+session_start();
 include("session.php");
-$idtrajet=$_GET["idtrajet"];
-$date=$_GET["date"];
-$place=$_GET["place"];
-echo($place);
-
+include("condb.php");
+$idtrajet=mysqli_real_escape_string( $conn,htmlspecialchars($_GET["idtrajet"]));
+$date=mysqli_real_escape_string( $conn,htmlspecialchars($_GET["date"]));
+$place=mysqli_real_escape_string( $conn,htmlspecialchars($_GET["place"]));
+//echo($date);
+$sql="UPDATE trajet SET date_depart='$date',nbplace='$place',nbplacer='$place' WHERE (idtrajet = '$idtrajet')";
+//echo($sql);
+$re=$conn->query($sql);
+$sql="SELECT idtrajet,pick_att,pick_long,drop_att,drop_long from trajet where idtrajet=$idtrajet";
+//echo $sql;
+$re=$conn->query($sql);
+$r=$re->fetch_assoc();
+var_dump($r);
 ?>
